0a5cd6f0235f5477b8c9e13ee2a4ea9b64c4a5c1
Tomasz Walas <tomasz.walass@gmail.com>
2022-12-01 09:36:28 +0100

    Solution for Day 1

---
+++ day01.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"sort"
+	"strconv"
+)
+
+func main() {
+	file, err := os.Open("./day01.txt")
+	if err != nil {
+		fmt.Println("Error while reading file")
+		return
+	}
+	defer file.Close()
+
+	list := []string{}
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		list = append(list, scanner.Text())
+	}
+
+	fmt.Println(PartOne(list))
+	fmt.Println(PartTwo(list))
+}
+
+func PartOne(list []string) int {
+	elfs := getElfsWithSnacks(list)
+	return elfs[len(elfs)-1]
+}
+
+func PartTwo(list []string) int {
+	elfs := getElfsWithSnacks(list)
+	top := elfs[len(elfs)-3:]
+	sum := 0
+
+	for _, v := range top {
+		sum += v
+	}
+
+	return sum
+}
+
+func getElfsWithSnacks(list []string) []int {
+	elfs := []int{0}
+	idx := 0
+	for _, v := range list {
+		if v == "" {
+			elfs = append(elfs, 0)
+			idx++
+			continue
+		}
+
+		number, err := strconv.Atoi(v)
+		if err != nil {
+			continue
+		}
+		elfs[idx] += number
+
+	}
+
+	sort.Ints(elfs)
+	return elfs
+
+}
---
+++ day01_test.go
@@ -0,0 +1,23 @@
+package main
+
+import "testing"
+
+func TestPartOne(t *testing.T) {
+	list := []string{"10", "20", "", "50", "", "30", "40"}
+	expected := 70
+	result := PartOne(list)
+
+	if result != expected {
+		t.Errorf("Expected %d got %d", expected, result)
+	}
+}
+
+func TestPartTwo(t *testing.T) {
+	list := []string{"10", "20", "", "50", "", "30", "40", "", "5", "5", "10", "", "1", "1", "1"}
+	expected := 150
+	result := PartTwo(list)
+
+	if result != expected {
+		t.Errorf("Expected %d got %d", expected, result)
+	}
+}
